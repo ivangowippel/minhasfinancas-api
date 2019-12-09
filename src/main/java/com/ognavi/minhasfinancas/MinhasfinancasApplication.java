10d451fc2d96d7f1d301f192c31235df1e9d468d
Ivango <ognavi@gmail.com>
2019-12-09 18:52:31 -0300

    adicionado comentário no arquivo principal da aplicacao

--- src/main/java/com/ognavi/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/ognavi/minhasfinancas/MinhasfinancasApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MinhasfinancasApplication {
 
 	public static void main(String[] args) {
+		// Sobe a aplicacao.
 		SpringApplication.run(MinhasfinancasApplication.class, args);
 	}
 
