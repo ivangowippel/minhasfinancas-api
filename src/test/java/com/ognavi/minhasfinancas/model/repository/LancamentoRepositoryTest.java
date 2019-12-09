8ca8d52c8c46b9d2c418de9dc2e4e2f9cc5ccc33
Ivango <ognavi@gmail.com>
2019-12-09 18:47:33 -0300

    first commit

---
+++ src/test/java/com/ognavi/minhasfinancas/model/repository/LancamentoRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.ognavi.minhasfinancas.model.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest	// é utilizado para testes de integração
+@AutoConfigureTestDatabase(replace = Replace.NONE)	//serve para não sobre escrever as configurações de teste
+@ActiveProfiles("test")
+public class LancamentoRepositoryTest {
+
+	@Autowired	//injeta o LancamentoRepository
+	LancamentoRepository repository;
+	
+	@Autowired
+	TestEntityManager entityManager;
+	
+	@Test
+	public void deveSalvarUmLancamento() {
+		
+		
+	}
+	
+}
