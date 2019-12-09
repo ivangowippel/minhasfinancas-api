cd9144af2c6d9dbbedb0a26c36cf9ba483bc2695
ivangowippel <ognavi@gmail.com>
2019-12-09 17:50:14 -0300

    Concluindo o controller de Lancamentos

---
+++ src/main/java/com/ognavi/minhasfinancas/api/dto/AtualizaStatusDTO.java
@@ -0,0 +1,13 @@
+package com.ognavi.minhasfinancas.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AtualizaStatusDTO {
+
+	private String status;
+}
